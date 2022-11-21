587acfa4df810507f2264525ff985176745a7781
myvega-2k <vega2k_@daum.net>
2022-11-21 14:47:53 +0900

    controller update

--- src/main/java/com/asianaidt/myrestapi/lectures/LectureController.java
+++ src/main/java/com/asianaidt/myrestapi/lectures/LectureController.java
@@ -1,5 +1,7 @@
 package com.asianaidt.myrestapi.lectures;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
@@ -12,15 +14,24 @@ import java.net.URI;
 
 @RestController
 @RequestMapping(value="/api/lectures", produces = MediaTypes.HAL_JSON_VALUE)
+@RequiredArgsConstructor
 public class LectureController {
+    //@Autowired
+    private final LectureRepository lectureRepository;
+
+    //constructor injection
+//    public LectureController(LectureRepository lectureRepository) {
+//        this.lectureRepository = lectureRepository;
+//    }
+
     @PostMapping
     public ResponseEntity createLecture(@RequestBody Lecture lecture) {
-        lecture.setId(10);
+        Lecture addLecture = lectureRepository.save(lecture);
 
         //http://localhost:8080/api/lectures/10
         WebMvcLinkBuilder selfLinkBuilder = WebMvcLinkBuilder.linkTo(LectureController.class).slash(lecture.getId());
         URI createUri = selfLinkBuilder.toUri();
         //created() - 201 code
-        return ResponseEntity.created(createUri).body(lecture);
+        return ResponseEntity.created(createUri).body(addLecture);
     }
 }
