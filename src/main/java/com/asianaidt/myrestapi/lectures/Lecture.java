e4a73dde7b8e9c19a168d2feaffc5797c7eb2cbd
myvega-2k <vega2k_@daum.net>
2022-11-21 14:18:49 +0900

    entity config

--- src/main/java/com/asianaidt/myrestapi/lectures/Lecture.java
+++ src/main/java/com/asianaidt/myrestapi/lectures/Lecture.java
@@ -1,7 +1,9 @@
 package com.asianaidt.myrestapi.lectures;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @Builder
@@ -10,19 +12,31 @@ import java.time.LocalDateTime;
 @Getter
 @Setter
 @EqualsAndHashCode(of="id")
+@Entity
 public class Lecture {
+    @Id @GeneratedValue(strategy = GenerationType.AUTO)
     private Integer id;
+
+    @Column(nullable = false)
     private String name;
     private String description;
+
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm")
     private LocalDateTime beginEnrollmentDateTime;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm")
     private LocalDateTime closeEnrollmentDateTime;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm")
     private LocalDateTime beginLectureDateTime;
+    @JsonFormat(pattern="yyyy-MM-dd HH:mm")
     private LocalDateTime endLectureDateTime;
+
     private String location;
     private int basePrice;
     private int maxPrice;
     private int limitOfEnrollment;
     private boolean offline;
     private boolean free;
+
+    @Enumerated(EnumType.STRING)
     private LectureStatus lectureStatus = LectureStatus.DRAFT;
 }
