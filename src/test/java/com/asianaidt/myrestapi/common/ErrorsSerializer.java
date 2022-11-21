b3fa7db6bf243d1df165afb4a8599258c2907bb0
myvega-2k <vega2k_@daum.net>
2022-11-21 21:36:16 +0900

    ErrosSerializer add

---
+++ src/test/java/com/asianaidt/myrestapi/common/ErrorsSerializer.java
@@ -0,0 +1,50 @@
+package com.asianaidt.myrestapi.common;
+
+import java.io.IOException;
+
+import org.springframework.boot.jackson.JsonComponent;
+import org.springframework.validation.Errors;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+
+@JsonComponent
+public class ErrorsSerializer extends JsonSerializer<Errors>{
+	@Override
+	public void serialize(Errors errors, JsonGenerator gen, SerializerProvider serializers) throws IOException {
+		gen.writeStartArray();
+        errors.getFieldErrors() //List<FieldError>
+                .forEach(e -> {
+            try {
+                gen.writeStartObject();
+                gen.writeStringField("field", e.getField());
+                gen.writeStringField("objectName", e.getObjectName());
+                gen.writeStringField("code", e.getCode());
+                gen.writeStringField("defaultMessage", e.getDefaultMessage());
+                Object rejectedValue = e.getRejectedValue();
+                if (rejectedValue != null) {
+                    gen.writeStringField("rejectedValue", rejectedValue.toString());
+                }
+                gen.writeEndObject();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        });
+
+        errors.getGlobalErrors() //List<ObjectError>
+                .forEach(e -> {
+            try {
+                gen.writeStartObject();
+                gen.writeStringField("objectName", e.getObjectName());
+                gen.writeStringField("code", e.getCode());
+                gen.writeStringField("defaultMessage", e.getDefaultMessage());
+                gen.writeEndObject();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        });
+        gen.writeEndArray();
+		
+	}
+}
