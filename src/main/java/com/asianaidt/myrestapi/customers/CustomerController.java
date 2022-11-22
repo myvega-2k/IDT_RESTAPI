b91ec1aad1dbbaaa432a83c9df4499474c79319e
myvega-2k <vega2k_@daum.net>
2022-11-22 16:10:57 +0900

    controller update

--- src/main/java/com/asianaidt/myrestapi/customers/CustomerController.java
+++ src/main/java/com/asianaidt/myrestapi/customers/CustomerController.java
@@ -1,9 +1,12 @@
 package com.asianaidt.myrestapi.customers;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/customers")
@@ -12,10 +15,14 @@ public class CustomerController {
     private CustomerService customerService;
 
     @PostMapping
-    public CustomerEntity addCustomer(@RequestBody CustomerEntity customer)
+    public ResponseEntity<?> addCustomer(@RequestBody CustomerEntity customer)
             throws Exception {
-
-        return customerService.insertCustomer(customer);
+        Optional<CustomerEntity> optional = customerService.selectCustomerByEmail(customer.getEmail());
+        if(optional.isPresent()){
+            return ResponseEntity.status(HttpStatus.CONFLICT).body("요청하신 " + customer.getEmail()+"이미 존재합니다.");
+        }
+        CustomerEntity addedCustomer = customerService.insertCustomer(customer);
+        return ResponseEntity.ok(addedCustomer);
     }
 
     @GetMapping
