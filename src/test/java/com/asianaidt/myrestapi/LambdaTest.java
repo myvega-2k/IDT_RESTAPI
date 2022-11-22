8b23acb4d44450b9b00e213d14350328cf8492b0
myvega-2k <vega2k_@daum.net>
2022-11-22 09:09:10 +0900

    consumer add

--- src/test/java/com/asianaidt/myrestapi/LambdaTest.java
+++ src/test/java/com/asianaidt/myrestapi/LambdaTest.java
@@ -1,9 +1,28 @@
 package com.asianaidt.myrestapi;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+import java.util.function.Consumer;
+
 public class LambdaTest {
+
     @Test
+    public void consumer() {
+        //Immutable List 
+        List<String> strList = List.of("java","boot","cloud");
+        strList.forEach(new Consumer<String>() {
+            @Override
+            public void accept(String s) {
+                System.out.println("s = " + s);
+            }
+        });
+
+    }
+    
+    
+    @Test @Disabled
     public void runnable() {
         //Runnable 인터페이스
         //1. Anonymous Inner class
