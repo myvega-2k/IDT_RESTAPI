1f82f88a52fef3d4e563e4d5481e6646105a013e
myvega-2k <vega2k_@daum.net>
2022-11-22 15:27:48 +0900

    custom entity update

--- src/main/java/com/asianaidt/myrestapi/customers/CustomerEntity.java
+++ src/main/java/com/asianaidt/myrestapi/customers/CustomerEntity.java
@@ -1,4 +1,23 @@
 package com.asianaidt.myrestapi.customers;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "customer")
+@Getter
+@Setter
 public class CustomerEntity {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private int age;
 }
