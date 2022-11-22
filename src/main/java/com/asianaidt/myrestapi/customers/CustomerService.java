22f77cec375f0586fb10cd11fe2b8ca21c640b2d
myvega-2k <vega2k_@daum.net>
2022-11-22 15:35:53 +0900

    service add

---
+++ src/main/java/com/asianaidt/myrestapi/customers/CustomerService.java
@@ -0,0 +1,19 @@
+package com.asianaidt.myrestapi.customers;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class CustomerService {
+    private final CustomerRepository customerRepository;
+
+    public CustomerEntity insertCustomer(CustomerEntity entity) {
+        return customerRepository.save(entity);
+    }
+
+
+
+}
